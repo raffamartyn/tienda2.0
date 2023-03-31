653472e8ca6c1910f6f6e799f6c1eebd1f8c9e69
raffamartyn <raffamartyn@gmail.com>
2023-03-30 22:00:33 -0300

    actualizacion2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
+import React from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App: React.FC <AppProps>=({ Component, pageProps })=> {
+  return (
+    
+  <ChakraProvider>
+        
+           <Component {...pageProps} />
+        
+    </ChakraProvider>
+   
+   
+  )
 }
+
+export default App;
